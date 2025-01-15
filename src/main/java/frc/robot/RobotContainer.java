de296e5ef93503a92a50a74c0fc9c1e3647443c5
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2025-01-15 16:42:42 -0700

    message
    s

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,186 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.TeleopSwerve;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.Swerve;
+
+public class RobotContainer {
+    //LED subsystem
+    private final LEDSubsystem m_LedSubsystem = new LEDSubsystem();
+
+    //Swerve
+    private static final Swerve s_Swerve = new Swerve();
+
+    public static double[] getRobotVector() {
+        return s_Swerve.getSwerveVector();
+    }
+    //limelight
+
+    //AUTOS
+    private SendableChooser<Command> autoChooser;
+    private final ParallelCommandGroup lockGroup = new ParallelCommandGroup(null);
+    private final SequentialCommandGroup indexerCmd = new SequentialCommandGroup(null);
+    private final SequentialCommandGroup indexerStopCommand = new SequentialCommandGroup(null);
+
+
+    //ShuffleboardTab limelightTab;
+
+    //joysticks
+
+    public final static Joystick driver = new Joystick(0);
+
+    public final static Joystick manipulator = new Joystick(1);
+
+
+    // Controller Buttons Used
+
+    // **********************************************************
+    // Xbox Controller - 1/Driver - Port 0
+    // ----------------------------------------------------------
+    // Left Stick (Translate along X and Y plane) - "Swerve"
+    // Right Stick (Rotate about Z-Axis) - Drive Right/Left
+    // Button Y - Zero the gyro
+    // Left Bumper - Robot Centric drive mode
+    // Right Bumper - Speed throttle
+    //
+    // **********************************************************
+
+    // **********************************************************
+    // Xbox Controller - 2/Operator - Port 1
+    // ----------------------------------------------------------
+    // Left Trigger -
+    // Right Trigger -
+    // Left Bumper
+    // Button A - Normal Shoot
+    // Button B - Slow Shoot
+    // Button X - Reverse Shooter
+    // Button Y - Aim limelight
+    // D-Pad up - intake & indexer in
+    // D-Pad Down - intake & indexer out
+    // **********************************************************
+
+
+    //Timer  rumbleTimer = new Timer();
+
+    //Driver Buttons
+    //Driver Buttons
+    /* Drive Controls */
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Driver Buttons */
+    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
+    private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kX.value);
+    public final static JoystickButton slowMove = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+
+
+    //manip
+    public final static JoystickButton aim = new JoystickButton(manipulator, XboxController.Button.kRightBumper.value);
+    public final static JoystickButton autoIntake = new JoystickButton(manipulator, XboxController.Button.kLeftBumper.value);
+    public final JoystickButton shooterReverse = new JoystickButton(manipulator, XboxController.Button.kB.value);
+    public final JoystickButton slowShoot = new JoystickButton(manipulator, XboxController.Button.kX.value);
+    public final JoystickButton ShootLimeLight = new JoystickButton(manipulator, XboxController.Button.kY.value);
+
+    
+    //intake
+    //public final JoystickButton intakeIn = new JoystickButton(manipulator, XboxController.Button.kLeftBumper.value);
+    // public final JoystickButton intakeOut = new JoystickButton(manipulator, XboxController.Button.kRightBumper.value);
+    //public final JoystickButton intakeSlow = new JoystickButton(manipulator, XboxController.Button.kA.value);
+
+    //indexer
+    //public final JoystickButton indexerIn = new JoystickButton(manipulator, XboxController.Button.kB.value);
+    //public final JoystickButton indexerOut = new JoystickButton(manipulator, XboxController.Button.kY.value);
+
+    //shooter
+
+    //public final JoystickButton shooterRun = new JoystickButton(manipulator, XboxController.Button.kA.value);
+
+    //private RotationSource hijackableRotation = new JoystickLock(); // get rotation from driver input;
+
+
+    public RobotContainer() {
+        //CommandScheduler.getInstance().registerSubsystem(m_LedSubsystem);
+        
+
+        s_Swerve.setDefaultCommand(
+
+                new TeleopSwerve(
+                        s_Swerve,
+                        () -> -driver.getRawAxis(translationAxis),
+                        () -> -driver.getRawAxis(strafeAxis),
+                        () -> -driver.getRawAxis(rotationAxis),
+                        () -> robotCentric.getAsBoolean(),
+                        () -> aim.getAsBoolean()
+                )
+        );
+
+
+        configureButtonBindings();
+        configureDefaultCommands();
+
+
+        //limelightTab = Shuffleboard.getTab("Limelight Tab");
+        //L_Limelight.configLimelightTab(limelightTab);
+        //L_Limelight.getShuffleboardValues();
+
+
+    }
+
+    private void configureBindings() {
+
+        /* AUTO STUFF  */
+        // SmartDashboard.putData("Blue Auto Score Twice", new PathPlannerAuto("Blue 2 Auto"));
+        //SmartDashboard.putData("Test", new PathPlannerAuto("Test"));
+        //SmartDashboard.putData("Example", new PathPlannerAuto("New Auto"));
+
+        // Add a button to run the example auto to SmartDashboard, this will also be in the auto chooser built above
+        // SmartDashboard.putData("Example Auto", new PathPlannerAuto("New Auto"));
+
+    }
+
+
+    // Add a button to run pathfinding commands to SmartDashboard
+
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+
+        /* Driver BUTTons */
+
+        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
+        /* Manipulator Butoons */
+
+
+       
+
+
+        //new JoystickButton(driver, XboxController.Button.kY.value)
+        //.onTrue(new InstantCommand(()   -> hijackableRotation =  new AprilTagLock()))
+        //.onFalse(new InstantCommand(() -> hijackableRotation = new JoystickLock()));
+
+    }
+
+    public void configureDefaultCommands() {
+       
+    }
+
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+
+    }
+}
